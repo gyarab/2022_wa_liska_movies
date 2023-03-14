7d901ca4afd0fa6a5510346746333a3a3d5868b2
Foxik <josef.liska@student.gyarab.cz>
2023-03-14 11:16:02 +0100

    L

--- gamdb/films/admin.py
+++ gamdb/films/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Movie
-admin.site.register(Movie)
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'year']
+    list_display_links = ['name']
+    pass
+
+admin.site.register(Movie, MovieAdmin)
